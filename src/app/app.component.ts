385daec131031459759bc565c5eddd8a3e6af71a
Abraham Vicioso <abrahanvicioso@gmail.com>
2024-12-17 21:09:14 -0400

    UPDATED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApiService } from './Services/api.service';
-
-
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',  
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'UCEFLIX';
-  constructor(private apiService: ApiService){
-
-  }
-
-  GetPelis(){
-
-    this.apiService.getPelis().subscribe((response) => {
-      console.log(response);
-    });
-  }
+  title = 'uceflix';
 }
